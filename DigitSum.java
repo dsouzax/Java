fd7be33122a8c8c3d79f9e496d5bdc4c9d99244b
Xavier <xavidsouza54@gmail.com>
2020-08-02 17:46:43 +0530

    Finding sum of digits

--- DigitSum.java
+++ DigitSum.java
@@ -1,12 +1,11 @@
-//Write a method with the name sumDigits that has one int parameter called number.
-//
-//If parameter is >= 10 then the method should process the number and return sum of all digits, otherwise return -1 to indicate an invalid value.
-//
-//The numbers from 0-9 have 1 digit so we donÅft want to process them, also we donÅft want to process negative numbers, so also return -1 for negative numbers.
-//
-//For example calling the method sumDigits(125) should return 8 since 1 + 2 + 5 = 8.
-//
-//Calling the method sumDigits(1) should return -1 as per requirements described above.
+/*
+    Write a method with the name sumDigits that has one int parameter called number.
+    If parameter is >= 10 then the method should process the number and return sum of all digits, otherwise return -1 to indicate an invalid value.
+    The numbers from 0-9 have 1 digit so we donÅft want to process them, also we donÅft want to process negative numbers, so also return -1 for negative numbers.
+
+    For example calling the method sumDigits(125) should return 8 since 1 + 2 + 5 = 8.
+        Calling the method sumDigits(1) should return -1 as per requirements described above.
+ */
 
 
  class DigitSum {
